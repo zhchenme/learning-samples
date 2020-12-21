2cd350f10480a03f6c652ba8a21222afded8c2ec
lanxiang <coderjas@foxmail.com>
2020-12-21 17:16:38 +0800

    CompletableFuture samples

---
+++ concurrency-samples/src/main/java/com/zhchen/concurrency/future/CompletableFutureSamples.java
@@ -0,0 +1,65 @@
+package com.zhchen.concurrency.future;
+
+import java.util.concurrent.*;
+
+import static org.junit.Assert.*;
+
+/**
+ * @author <a href="mailto:chen.zhang@yunhuyj.com">lanxiang</a>
+ * @since 2020/12/18
+ */
+public class CompletableFutureSamples {
+
+    static ExecutorService executor = Executors.newFixedThreadPool(3, new ThreadFactory() {
+        int count = 1;
+
+        @Override
+        public Thread newThread(Runnable runnable) {
+            return new Thread(runnable, "custom-executor-" + count++);
+        }
+    });
+
+    public static void main(String[] args) {
+
+        // completedFutureExample();
+        runAfterBothExample();
+    }
+
+    static void runAfterBothExample() {
+        String original = "Message";
+        StringBuilder result = new StringBuilder();
+        CompletableFuture.completedFuture(original).thenApply(String::toUpperCase).runAfterBoth(
+                CompletableFuture.completedFuture(original).thenApply(String::toLowerCase),
+                () -> result.append("done"));
+        assertTrue("Result was empty", result.length() > 0);
+    }
+
+    static void thenApplyAsyncWithExecutorExample() {
+        CompletableFuture<String> cf = CompletableFuture.completedFuture("message").thenApplyAsync(s -> {
+            assertTrue(Thread.currentThread().getName().startsWith("custom-executor-"));
+            assertFalse(Thread.currentThread().isDaemon());
+            return s.toUpperCase();
+        }, executor);
+
+        assertNull(cf.getNow(null));
+        assertEquals("MESSAGE", cf.join());
+        executor.shutdownNow();
+    }
+
+    static void thenApplyAsyncExample() {
+        CompletableFuture<String> cf = CompletableFuture.completedFuture("message").thenApplyAsync(s -> {
+            assertTrue(Thread.currentThread().isDaemon());
+            return s.toUpperCase();
+        });
+        assertNull(cf.getNow(null));
+        assertEquals("MESSAGE", cf.join());
+    }
+
+    static void completedFutureExample() {
+        CompletableFuture<String> cf = CompletableFuture.completedFuture("message");
+        assertTrue(cf.isDone());
+        assertEquals("message", cf.getNow(null));
+    }
+
+
+}
